ffd2a647ef1c149f08a0d10749028922da5cb809
jalkae <jlkshuen@gmail.com>
2023-02-22 13:59:02 +0800

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+ 
+public class GameManager : MonoBehaviour
+{
+    public Text healthText;
+    public int health;
+    public Text diedText;
+    public Text roundText;
+    public Text roundTitle;
+    public int round;
+    public Player player;
+    public GameObject Round1;
+    public GameObject Round2;
+    public GameObject Round3;
+    public GameObject Round4;
+    public GameObject Round5;
+    public GameObject Round6;
+    public GameObject Round7;
+    public GameObject Round8;
+    public Vector2 startPos;
+    public GameObject ShadeWin;
+    public Text WonText;
+    public Text EText;
+   
+    private void Awake()
+    {
+        health = 10;
+        diedText.enabled = false;
+        player.enabled = true;
+        round = 1;
+        Round1.SetActive(true);
+        Round2.SetActive(false);
+        Round3.SetActive(false);
+        Round4.SetActive(false);
+        Round5.SetActive(false);
+        Round6.SetActive(false);
+        Round7.SetActive(false);
+        Round8.SetActive(false);
+        ShadeWin.SetActive(false);
+        EText.enabled = true;
+        WonText.enabled = false;
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+        
+    }
+    public void DecreaseHealth()
+    {
+        health--;
+        healthText.text = health.ToString();
+        
+        
+    }
+    public void RoundSys()
+    {
+        EText.enabled=false;
+        health = 10;
+        healthText.text = health.ToString();
+        round++;
+        roundText.text = round.ToString();
+        
+        
+        switch(round)
+        {
+            case 2: 
+            Round1.SetActive(false);
+            Round2.SetActive(true);
+            break;
+
+            case 3: 
+            Round2.SetActive(false);
+            Round3.SetActive(true);
+            break;
+
+            case 4: 
+            Round3.SetActive(false);
+            Round4.SetActive(true);
+            break;
+
+            case 5: 
+            Round4.SetActive(false);
+            Round5.SetActive(true);
+            break;
+
+            case 6: 
+            Round5.SetActive(false);
+            Round6.SetActive(true);
+            break;
+
+            case 7: 
+            Round6.SetActive(false);
+            Round7.SetActive(true);
+            break;
+
+            case 8: 
+            roundTitle.text = "Final Round";
+            roundText.text = "";
+            Round7.SetActive(false);
+            Round8.SetActive(true);
+            break;
+
+            case 9:
+            roundTitle.text = "";
+            roundText.text = "";
+            ShadeWin.SetActive(true);
+            WonText.enabled = true;
+            player.enabled = false;
+            break;
+
+            default:
+            break;
+        }
+       
+    }
+    public void Died()
+    {
+        if(health == 0)
+        {
+            Pause();
+            diedText.enabled = true;
+        }
+        
+    }
+   
+    
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb2D;
+    private float moveSpeed;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
+    private float jumpForce;
+    
+    
+    
+    public bool iFrame;
+    public float iFrameSec;
+    public Vector2 startPos;
+
+    private void Awake()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        
+        
+    }
+    private void Start()
+    {
+        moveSpeed = 3f;
+        jumpForce = 60f;
+        isJumping = false;
+        iFrame = false;
+        iFrameSec = 1.5f;
+        
+        startPos = transform.position;
+      
+    } 
+    private void Update()
+    {
+         
+        moveHorizontal = Input.GetAxisRaw("Horizontal"); // default in unity settings is A(Left = -1) and D(Right = 1) or left and right arrow (No keypress = 0)
+        moveVertical = Input.GetAxisRaw("Vertical"); //default is W S Up Down
+        
+         
+    }
+
+    private void FixedUpdate() //similar to Update but used when working with physics
+    {
+        if(moveHorizontal > 0.1f || moveHorizontal < -0.1f) // not 0 since keypress is 0 which may cause confusion
+        {
+            rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse); //Time.deltaTime is not used since AddForce has it applied as default in ForceMode
+        }
+
+        if(!isJumping && moveVertical > 0.1f) // not 0 since keypress is 0 which may cause confusion
+        {
+            rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Complete")
+        {
+            Debug.Log("Player Entered");
+        }
+    }
+    
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+            
+        }
+     
+        
+        if(collision.gameObject.tag == "Complete" && Input.GetKeyDown(KeyCode.E))
+        {
+            
+            Debug.Log("Level Completed!");
+            FindObjectOfType<GameManager>().RoundSys();
+            transform.position = startPos;
+            
+            
+        }
+       
+        if(!iFrame && collision.gameObject.tag == "Obstacle")
+            {
+            FindObjectOfType<GameManager>().DecreaseHealth();   
+            
+            StartCoroutine(IFrames());
+            StartCoroutine(IFrameBlink());
+            FindObjectOfType<GameManager>().Died();
+            }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ground") 
+        {
+            isJumping = true;
+        }
+       
+        if(collision.gameObject.tag == "Complete")
+        {
+            Debug.Log("Player Exited");
+        }
+    }
+    public IEnumerator IFrames()
+    {
+         
+        Debug.Log("Player Invinsible!");
+        iFrame = true;
+        
+        yield return new WaitForSeconds(iFrameSec);
+        
+        
+        iFrame = false;
+        Debug.Log("Player Vulnerable!");
+         
+    }
+    public IEnumerator IFrameBlink()
+    {
+        while(iFrame==true && FindObjectOfType<GameManager>().health > 0)
+        {
+            yield return new WaitForSeconds(0.05f);
+            spriteRenderer.enabled=false;
+            yield return new WaitForSeconds(0.05f);
+            spriteRenderer.enabled=true;
+            yield return null;
+        }
+         
+    }
+     
+}
